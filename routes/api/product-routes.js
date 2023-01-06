0abd74beea07030acb30506b2bc681a3835613dc
Mark Calcagno <mcalcagn@gmail.com>
2023-01-05 20:36:20 -0600

    finished product-routes? Will test when code is completed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -41,13 +41,13 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: req.body.username,
+      product_name: req.body.product_name,
       price: req.body.price,
       stock: req.body.stock,
       tagIds: req.body.tagIds
     }
   */
-  Product.create(req.body)
+  Product.create(req.body) 
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
