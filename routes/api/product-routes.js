740d4111309b66b289c43eecebe5d7101cdcfdc7
Mark Calcagno <mcalcagn@gmail.com>
2023-01-05 19:24:27 -0600

    added delete feature

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -41,10 +41,10 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      product_name: req.body.username,
+      price: req.body.price,
+      stock: req.body.stock,
+      tagIds: req.body.tagIds
     }
   */
   Product.create(req.body)
@@ -113,6 +113,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
