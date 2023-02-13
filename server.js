8e8bb9cee5bef6e60eacfe008cb1f1e41b5d2e83
Mark Calcagno <mcalcagn@gmail.com>
2023-02-12 19:42:11 -0600

    finished category-routes, first pass. Also updated server.js with sequelize, and the ProductTag model

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection');
+const path = require('path');
+
 // import sequelize connection
+
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {maxAge: 36000},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
