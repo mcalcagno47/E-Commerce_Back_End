6b2f213d07fb1f6ef109aa375184ff02fd67367f
Mark Calcagno <mcalcagn@gmail.com>
2023-02-14 18:56:15 -0600

    fixed server.js file. I added things when I shouldn't have

--- server.js
+++ server.js
@@ -1,31 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./config/connection');
-const path = require('path');
-
 // import sequelize connection
-
-const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {maxAge: 36000},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -33,10 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () =>
-    console.log(
-      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
-    )
-  );
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
