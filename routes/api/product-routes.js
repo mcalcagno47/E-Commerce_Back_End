741ce5e8fa98266efbb9bf4e1773a4e8468871df
Mark Calcagno <mcalcagn@gmail.com>
2023-02-14 22:04:24 -0600

    added assets. homework complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,11 +4,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findAll({
+    const productData = await Product.findAll(req.params.product_name, {
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
       include: [{ model: Category, attributes: ['category_name'] }, { model: Tag, attributes: ['tag_name'] }],
     });
@@ -19,11 +19,11 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findByPk(req.params.id, {
+    const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category, attributes: ['category_name'] }, { model: Tag, attributes: ['tag_name'] }],
     });
 
